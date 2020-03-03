79e7c0131505100231af89c267be35716813a013
Talha Abbasi <talhaabbasi1997@gmail.com>
2020-03-03 12:16:10 +0500

    Search Added

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    artistQuery: ""
+  };
+  updateArtistQuery = event => {
+    this.setState({ artistQuery: event.target.value });
+  };
+  handleKeyPress = event => {
+    if (event.key === "Enter") {
+      this.searchArtist();
+    }
+  };
+
+  searchArtist = () => {
+    console.log(this.state);
+  };
   render() {
-    return <div>React App</div>;
+    return (
+      <div>
+        <h2>Music Master</h2>
+        <input
+          onChange={this.updateArtistQuery}
+          onKeyPress={this.handleKeyPress}
+          placeholder="Search for an artist"
+        />
+        <button onClick={this.searchArtist}>Search</button>
+      </div>
+    );
   }
 }
 
